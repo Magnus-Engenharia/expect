f58c3d262bedd72b9b3700ba3a4a9d9dd003514b
Aiden Bai <aiden.bai05@gmail.com>
2026-03-15 22:09:39 -0700

    evans

--- evals/src/constants.ts
+++
@@ -1,6 +0,0 @@
-export const MUTATION_TIMEOUT_MS = 180_000;
-export const HMR_WAIT_MS = 2_000;
-export const HMR_REVERT_WAIT_MS = 1_000;
-export const VITE_STARTUP_TIMEOUT_MS = 30_000;
-export const VITE_POLL_INTERVAL_MS = 500;
-export const DEFAULT_CONCURRENCY = 1;
